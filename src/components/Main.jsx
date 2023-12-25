d4a7815498c2523fa126df8e92ec0ace342ea590
Shayanvatsa <shayanvatsa@gmail.com>
2023-12-25 23:51:33 +0530

    Initial commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import {TypeAnimation} from 'react-type-animation'
+import {FaLinkedinIn, FaInstagram } from 'react-icons/fa'
+
+const Main= () => {
+    return(
+        <div id='main'>
+            <img className='w-full h-screen object-cover 'src="https://images.unsplash.com/photo-1531297484001-80022131f5a1?q=80&w=2020&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="BG Image" style={{ filter: 'blur(6px)' }}/>
+            <div className= 'w-full h-screen absolute top-0 left-0 bg-black/30 '>
+            <div className='max-w-[700px] m-auto h-full flex flex-col justify-center lg:items-start items-center'>
+                <h1 className='sm:text-5xl text-4xl font-bold text-red-50'style= {{ margin: 0, fontSize: '3em', paddingLeft: '5px', color: '#edf5e1' }}>Hey,there! I'm Shayan Vatsa. </h1>
+                <h2 className='sm:text-3xl text-2xl pt-4 text-red-50'style= {{ margin: 0, fontSize: '2em', paddingLeft: '5px', color: '#edf5e1' }}>
+
+                <TypeAnimation
+      sequence={[
+        // Same substring at the start will only be typed out once, initially
+        'A Tech Enthusiast ',
+        1000, // wait 1s before replacing "Mice" with "Hamsters"
+        '& Cybersecurity Nerd.',
+        1000
+      ]}
+      wrapper="span"
+      speed={50}
+      style={{ fontSize: '1em', paddingLeft: '5px'}}
+      repeat={Infinity}
+    />
+    </h2>
+    <div className='flex justify-between pt-6 max-w-[200px] w-full'>
+        <FaLinkedinIn className='cursor-pointer'size={20} />
+        <FaInstagram className='cursor-pointer'size={20} />
+        
+    </div>
+    </div>
+        </div>
+        </div>
+    )
+}
+
+export default Main
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import { AiOutlineMenu, AiOutlineHome, AiOutlineProject, AiOutlineMail } from 'react-icons/ai';
+import { GrProjects } from 'react-icons/gr';
+import { FaCode } from "react-icons/fa6";
+import { GoPaperclip } from "react-icons/go";
+import { IoIosContact } from "react-icons/io";
+import { IoIosInformationCircleOutline } from "react-icons/io";
+
+const NavBar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  return (
+    <div>
+      <AiOutlineMenu
+        onClick={handleNav}
+        className='fixed top-4 right-4 z-[99] md:hidden' />
+        {
+          nav ?(
+            <div className='Fixed w-full h-screen bg-black/90 flex flex-col justify-center z-20'>
+              <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-red-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                <IoIosInformationCircleOutline size={20} />
+                <span className='pl-4'>About</span>
+              </a>
+              <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-red-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                <FaCode size={20} />
+                <span className='pl-4'>Skills</span>
+              </a>
+              <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-red-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                <GrProjects size={20} />
+                <span className='pl-4'>Projects</span>
+              </a>
+              <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-red-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                <GoPaperclip size={20} />
+                <span className='pl-4'>Resume</span>
+              </a>
+              <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-red-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                <IoIosContact  size={20} />
+                <span className='pl-4'>Contact</span>
+              </a>
+            </div>
+
+          )
+          :(
+            ''
+          )
+        }
+
+        <div className='md:block hidden fixed top-[25%] z-10'>
+          <div className='flex flex-col'>
+            <a href='#main' className='rounded-full shadow-lg bg-red-500 shadow-emerald-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+            <IoIosInformationCircleOutline size={25}/>
+            </a>
+            <a href='#skills' className='rounded-full shadow-lg bg-red-500 shadow-emerald-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+            <FaCode size={25}/>
+            </a>
+            <a href='#projects' className='rounded-full shadow-lg bg-red-500 shadow-emerald-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+            <GrProjects size={25}/>
+            </a>
+            <a href='#resume' className='rounded-full shadow-lg bg-red-500 shadow-emerald-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+            <GoPaperclip size={25}/>
+            </a>
+            <a href='#contact' className='rounded-full shadow-lg bg-red-500 shadow-emerald-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+            <IoIosContact size={25}/>
+            </a>
+          </div>
+        </div>
+
+    </div>
+  );
+};
+
+export default NavBar;
